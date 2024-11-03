f6a2a7006bdab0f4f7ffa69f21688786203d9d16
Kalpani1234 <Kalpani4lk@gmail.com>
2024-11-03 16:41:52 +0530

    edit

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -11,10 +11,9 @@ export const Home = () => {
                  <div className="absolute inset-0 flex items-center justify-center">
                   <h1 className="text-5xl font-bold text-white">
                   Explore Beyond the Map
-
+                  
                   </h1>
-                  </div>
-                 
+              </div>
             <Footer/>
         </div>
     )
