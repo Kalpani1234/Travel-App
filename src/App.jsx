949fed7ef1dbe4e36be3748123961ecb0b6e420f
Kalpani1234 <Kalpani4lk@gmail.com>
2024-11-03 15:25:20 +0530

    Change

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,15 @@ import './App.css'
 import {Home} from "./pages/home.jsx";
 import {Route, Routes} from "react-router-dom";
 import {Contact} from "./pages/contact.jsx";
+import {About} from "./pages/about.jsx";
+
 
 function App() {
   return (
     <>
         <Routes>
             <Route path={"/"} element={<Home />} />
+            <Route path={"/about"} element={<About />} />
             <Route path={"/contact"} element={<Contact />} />
         </Routes>
     </>
